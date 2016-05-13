db24ad5b34307c15d5a594e832a2962a23640301
kimxuyen <chibi_kyo@yahoo.com.vn>
2016-05-13 19:57:13 +0700

    done

---
+++ Calendar(jquery)/calendar.js
@@ -0,0 +1,11 @@
+﻿$(function() {
+	$("#inputDate").datepicker({
+		inline: true,
+		firstDay: 1,
+		showOtherMonths: true,
+		dayNames: ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"],
+		changeMonth: true,
+		changeYear: true,
+		showOn: 'both',
+	});
+});
