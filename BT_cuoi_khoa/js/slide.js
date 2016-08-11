d1c5d644b9994fcb7228d3dffbf4962dfdce190d
kimxuyen <chibi_kyo@yahoo.com.vn>
2016-08-11 16:43:39 +0700

    fix

--- BT_cuoi_khoa/js/slide.js
+++ BT_cuoi_khoa/js/slide.js
@@ -1,8 +1,45 @@
 //load
 $(document).ready(function() {
-	$('#slide img:gt(0)').hide();
-	setInterval(function() {
-		$('#slide :first-child').fadeOut("slow")
-			.next('img').fadeIn("slow")
-			.end().appendTo('#slide');},4000);
+	var alt = 0;
+	startImg = $(".slide-img:first").attr("alt");
+	endImg = $(".slide-img:last").attr("alt");
+	$(".slide-img").each(function() {
+		if ($(this).is(':visible')) {
+			 //attr: get value property
+			stt = $(this).attr("alt");
+		}
+	})
+	//next
+	$("#img-next").click(function() {
+		next = ++alt;
+		if (next > endImg) {
+			alt= startImg;
+		}
+		$(".slide-img").fadeOut();
+		//eq: index element
+		$(".slide-img").eq(alt).fadeIn();
+		$("input").removeClass("active-index");
+		$("input").eq(alt).addClass("active-index");
+	});
+
+	//previous
+	$("#img-prev").click(function() {
+		prev = --alt;
+		if (prev < startImg) {
+			alt = endImg;
+		}
+		$(".slide-img").fadeOut();
+		$(".slide-img").eq(prev).fadeIn();
+	});
+	//index 
+	$("input").click(function() {
+		var index = 0;
+		index = $("input").index(this);
+		alt = index;
+		$(".slide-img").fadeOut();
+		$(".slide-img").eq(alt).fadeIn();
+		$("input").removeClass("active-index");
+		$(this).addClass("active-index");
+	});
+	setInterval(function() {$("#img-next").click()},4000); 
 });
